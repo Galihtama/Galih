6c7ba7a2e2ec57d996b9d30be541a6606f5ea64a
Galihtama <pratamagalihrd@gmail.com>
2024-02-08 01:05:49 +0700

    mencoba menambahkan remove saat klik tombol diluar hamburger atau nav ul akan menutup dengan sendirinya

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector ('nav ul');
+const nav = document.querySelector ('.slide');
+// cont menu = document.querySelector ('.menu-toggle')
 
 
 menuToggle.addEventListener('click', function () {
-    nav.classList.toggle('slide');
-})
+    nav.classList.toggle('active');
+});
+
+
+    
+
+// klik diluar hamburger menu 
+
+
+
+// document.addEventListener('click', function (e) {
+//     if ( !menuToggle.contains(e.target) && !nav.contains(e.target)) {
+//         nav.classList.remove('active');
+//     }
+// });
